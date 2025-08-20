036755d31ecda00c0c5a5fdaa8e074873d306f16
Chitaoji <2360742040@qq.com>
2025-08-21 00:58:19 +0800

    Add error

--- src/tokenize.py
+++ src/tokenize.py
@@ -18,65 +18,77 @@ class UqToken(NamedTuple):
 
     type: str
     value: str
-    line: int
+    lineno: int
+    code: str
 
 
 class UqTokenizer:
     """Parser of UqTokens."""
 
     def __init__(self) -> None:
+        token_specification = [
+            ("NUM", r"\d+(\.\d*)?"),  # Integer or decimal numbers
+            ("DOUBLEARROW", r"=>"),  # Double arrows
+            ("ARROW", r"->"),  # Arrows
+            ("ASSIGN", r"="),  # Assignment operators
+            ("END", r";"),  # Statement terminators
+            ("ELLIPSIS", r"\.\.\."),  # Ellipsis
+            ("ID", r"[A-Za-z._]+"),  # Identifiers
+            ("OP", r"[+\-*/^]"),  # Arithmetic operators
+            ("NEWLINE", r"\n"),  # Line endings
+            ("SKIP", r"[ \t]+"),  # Skip over spaces and tabs
+            ("COMMENT", r"#.*"),  # Comments
+            ("LP", r"\("),  # Left parentheses
+            ("RP", r"\)"),  # Right parentheses
+            ("LS", r"\["),  # Left square brackets
+            ("RS", r"\]"),  # Right square brackets
+            ("LB", r"{"),  # Left braces
+            ("RB", r"}"),  # Right braces
+            ("DOUBLECOLON", r"::"),  # Double Colons
+            ("COLON", r":"),  # Colons
+            ("COMMA", r","),  # Commas
+            ("TYPEJOIN", r"\$"),  # Type join
+            ("MISMATCH", r"."),  # Any other character
+        ]
+        self.token_pattern = "|".join(
+            f"(?P<{pair[0]}>{pair[1]})" for pair in token_specification
+        )
+        self.keywords = {
+            "using",
+            "str",
+            "field",
+            "fac",
+            "int",
+            "float",
+            "bool",
+            "True",
+            "False",
+        }
         self.iter = iter(())
-
-    def parse_code(self, code: str) -> None:
-        """Parse the code."""
-        self.iter = chain(self.iter, tokenize(code))
 
     def consume(self) -> UqToken:
         """Consume a token."""
 
+    def parse_code(self, code: str) -> None:
+        """Parse the code."""
+        self.iter = chain(self.iter, self.tokenize(code))
 
-def tokenize(code: str) -> Iterator[UqToken]:
-    """Tokenize the code."""
-    keywords = {"using", "str", "field", "fac", "int", "float", "bool", "True", "False"}
-    token_specification = [
-        ("NUM", r"\d+(\.\d*)?"),  # Integer or decimal numbers
-        ("DOUBLEARROW", r"=>"),  # Double arrows
-        ("ARROW", r"->"),  # Arrows
-        ("ASSIGN", r"="),  # Assignment operators
-        ("END", r";"),  # Statement terminators
-        ("ELLIPSIS", r"\.\.\."),  # Ellipsis
-        ("ID", r"[A-Za-z._]+"),  # Identifiers
-        ("OP", r"[+\-*/^]"),  # Arithmetic operators
-        ("NEWLINE", r"\n"),  # Line endings
-        ("SKIP", r"[ \t]+"),  # Skip over spaces and tabs
-        ("COMMENT", r"#.*"),  # Comments
-        ("LP", r"\("),  # Left parentheses
-        ("RP", r"\)"),  # Right parentheses
-        ("LS", r"\["),  # Left square brackets
-        ("RS", r"\]"),  # Right square brackets
-        ("LB", r"{"),  # Left braces
-        ("RB", r"}"),  # Right braces
-        ("DOUBLECOLON", r"::"),  # Double Colons
-        ("COLON", r":"),  # Colons
-        ("COMMA", r","),  # Commas
-        ("TYPEJOIN", r"\$"),  # Type join
-        ("MISMATCH", r"."),  # Any other character
-    ]
-    tok_regex = "|".join(f"(?P<{pair[0]}>{pair[1]})" for pair in token_specification)
-    line_num = 1
-    for m in re.finditer(tok_regex, code):
-        ttype = m.lastgroup
-        value = m.group()
-        match ttype:
-            case "NUM":
-                value = float(value) if "." in value else int(value)
-            case "ID" if value in keywords:
-                ttype = value.upper()
-            case "NEWLINE":
-                line_num += 1
-                continue
-            case "SKIP":
-                continue
-            case "MISMATCH":
-                raise RuntimeError(f"{value!r} unexpected on line {line_num}")
-        yield UqToken(ttype, value, line_num)
+    def tokenize(self, code: str) -> Iterator[UqToken]:
+        """Tokenize the code."""
+        lineno = 1
+        for m in re.finditer(self.token_pattern, code):
+            ttype = m.lastgroup
+            value = m.group()
+            match ttype:
+                case "NUM":
+                    value = float(value) if "." in value else int(value)
+                case "ID" if value in self.keywords:
+                    ttype = value.upper()
+                case "NEWLINE":
+                    lineno += 1
+                    continue
+                case "SKIP":
+                    continue
+                case "MISMATCH":
+                    raise RuntimeError(f"{value!r} unexpected on line {lineno}")
+            yield UqToken(ttype, value, lineno, code)
